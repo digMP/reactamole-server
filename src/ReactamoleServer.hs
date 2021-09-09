58e205182a800b29316e0c8bf7be03c6b9decd98
Peter-Michael Osera <psosera@gmail.com>
2021-09-09 18:31:30 -0500

    added declarations to UI

--- src/ReactamoleServer.hs
+++ src/ReactamoleServer.hs
@@ -11,39 +11,68 @@ module Main where
 
 import Bio.Reactamole
 import Bio.Reactamole.Export
-
 import Data.Aeson
 import Data.Aeson.TH
-
+import Data.List (isPrefixOf)
 import Control.Monad.IO.Class
-
 import GHC.Generics
-
 import Network.Wai
 import Network.Wai.Handler.Warp
-
 import Servant
 import System.Environment
 
 import qualified Language.Haskell.Interpreter as Hint
+import qualified Data.List as L
+import qualified Data.Text as T
 
 ----- Server Logic {{{ ---------------------------------------------------------
 
-evalOne :: String -> IO (Either Hint.InterpreterError String)
-evalOne src = Hint.runInterpreter $ do
-    Hint.setImports ["Prelude", "Bio.Reactamole", "Bio.Reactamole.Export"]
-    Hint.eval src
+strip :: String -> String
+strip = T.unpack . T.strip . T.pack
+
+parseDecls :: String -> [String]
+parseDecls = collapse Nothing . map strip . lines
+  where
+    collapse :: Maybe String -> [String] -> [String]
+    collapse Nothing     [] = []
+    collapse (Just stmt) [] = [stmt]
+    collapse Nothing     (l:ls)
+      | "let" `isPrefixOf` l = collapse (Just l) ls
+      | otherwise            = l : collapse Nothing ls  -- N.B., bad, but Hint will catch it
+    collapse (Just stmt) (l:ls)
+      | "let" `isPrefixOf` l = stmt : collapse (Just l) ls
+      | otherwise            = collapse (Just $ stmt ++ " " ++ l) ls
+
+evalProg :: String -> String -> IO (Either Hint.InterpreterError String)
+evalProg decls expr = Hint.runInterpreter $ do
+  Hint.set [Hint.languageExtensions Hint.:= [Hint.GADTs]]
+  Hint.setImports ["Prelude", "Bio.Reactamole", "Bio.Reactamole.Export"]
+  mapM_ Hint.runStmt (parseDecls decls)
+  Hint.eval $ combineLines expr
+    where
+      combineLines :: String -> String
+      combineLines =
+        unwords
+        . filter (not . ("--" `isPrefixOf`))
+        . filter (not . null)
+        . map strip
+        . lines
 
 ----- }}} ----------------------------------------------------------------------
 
 ----- API+Server Definition {{{ ------------------------------------------------
+
+data EvaluateA = EvaluateA { decls :: String, expr :: String }
+  deriving Generic
+
+instance FromJSON EvaluateA
 
 data EvaluateR = EvaluateR { success :: Bool, output :: String }
   deriving Generic
 
 instance ToJSON EvaluateR
 
-type API = "evaluate" :> ReqBody '[PlainText] String :> Post '[JSON] EvaluateR
+type API = "evaluate" :> ReqBody '[JSON] EvaluateA :> Post '[JSON] EvaluateR
       :<|> "static" :> Raw
 
 app :: Application
@@ -55,9 +84,9 @@ api = Proxy
 server :: Server API
 server = result :<|> serveDirectoryWebApp "static"
   where
-    result :: String -> Handler EvaluateR
-    result src = do
-      result <- liftIO $ evalOne src
+    result :: EvaluateA -> Handler EvaluateR
+    result (EvaluateA decls expr) = do
+      result <- liftIO $ evalProg decls expr
       pure $ case result of
         Left err     -> EvaluateR False (show err)
         Right output -> EvaluateR True output
